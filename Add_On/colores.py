43177f59c65127c7851fa93f0da900026b23c97c
Abhay Pratap Singh <abhay.pratap_cs23@gla.ac.in>
2025-10-30 21:43:15 +0530

    Add colores.py to print color names

---
+++ Add_On/colores.py
@@ -0,0 +1,9 @@
+# these are all the colors...
+print("\033[31mRed")
+print("\033[32mGreen")
+print("\033[33mYellow")
+print("\033[34mBlue")
+print("\033[35mPurple")
+print("\033[36mCyan")
+print("\033[37mWhite")
+print("\033[0m")
